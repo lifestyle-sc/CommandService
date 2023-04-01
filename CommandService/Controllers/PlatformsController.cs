7a2f56d620974fd2d5491f209d82863f7174085b
Oluwakayode Momoh <68966387+lifestyle-sc@users.noreply.github.com>
2023-04-01 14:10:35 +0100

    created the platform controller

---
+++ CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService.Controllers
+{
+    [Route("/api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : Controller
+    {
+        public PlatformsController()
+        {
+            
+        }
+
+        [HttpPost]
+        public IActionResult TestBound()
+        {
+            return Ok("Welcome to platforms Controller!");
+        }
+    }
+}
